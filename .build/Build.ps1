0492a8d28e7713ee7ce695b90b70e76d2b8266e9
BrentSchmaltz <brentschmaltz@hotmail.com>
2016-05-27 09:36:23 -0700

    moved cli to dotnetcli folder

--- .build/Build.ps1
+++ .build/Build.ps1
@@ -1,5 +1,15 @@
 param([string]$build="YES", [string]$buildConfiguration="Debug", [string]$installdotnet="YES", [string]$restore="YES", [string]$root=$PSScriptRoot, [string]$runtests="YES")
 
+function SetCoreFxVersion([string]$fileName, [string]$oldVersion, [string]$newVersion)
+{
+    Write-Host "fileName: " $fileName;
+    Write-Host "oldVersion: " $oldVersion;
+    Write-Host "newVersion: " $newVersion;
+    $content = Get-Content -Path $fileName
+    $newContent = $content -replace $oldVersion, $newVersion
+    Set-Content $fileName $newContent
+}
+
 Write-Host ""
 Write-Host "============================"
 Write-Host "build.ps1"
@@ -15,17 +25,19 @@ Write-Host "PSScriptRoot: " $PSScriptRoot;
 
 $author = $buildConfiguration.SelectSingleNode("root/author").InnerText;
 $licenseUrl = $buildConfiguration.SelectSingleNode("root/licenseUrl").InnerText;
-$copyright = $buildConfiguration.SelectSingleNode("root/copyright").InnerText;
+$cliChannel =  $buildConfiguration.SelectSingleNode("root/cliChannel").InnerText;
 $cliVersionWin = $buildConfiguration.SelectSingleNode("root/cliVersionWin").InnerText;
+$cliLocalInstallFolder = "$PSScriptRoot\dotnetcli\local\" + $cliVersionWin;
+$copyright = $buildConfiguration.SelectSingleNode("root/copyright").InnerText;
 $coreFxVersion = $buildConfiguration.SelectSingleNode("root/coreFxVersion").InnerText;
 $nugetVersion = $buildConfiguration.SelectSingleNode("root/nugetVersion").InnerText;
-$cliLocalInstallFolder = "$PSScriptRoot\dotnet\local\" + $cliVersionWin;
 $dotnetexe = "$cliLocalInstallFolder\dotnet.exe";
 
 Write-Host ""
 Write-Host "============================"
 Write-Host "author: " $author;
 Write-Host "copyright: " $copyright;
+Write-Host "cliChannel: " $cliChannel;
 Write-Host "cliLocalInstallFolder: " $cliLocalInstallFolder;
 Write-Host "cliVersionWin: " $cliVersionWin;
 Write-Host "coreFxVersion: " $coreFxVersion;
@@ -38,11 +50,25 @@ if ($installdotnet -eq "YES")
     Write-Host ""
     Write-Host "============================"
     Write-Host "Install donetcli"
-    Write-Host "dotnetVersion = $dotnetVersion"
+    Write-Host "cliVersionWin = $cliVersionWin"
     Write-Host "dotnetLocalInstallFolder = $cliLocalInstallFolder"
     Write-Host "dotnetexe = $dotnetexe"
     Write-Host ""
-    &$PSScriptRoot\dotnet\install.ps1 -Channel "beta" -Version $dotnetVersion -Architecture x64 -InstallDir $cliLocalInstallFolder
+    &$PSScriptRoot\dotnetcli\install.ps1 -Channel $cliChannel -Version $cliVersionWin -Architecture x64 -InstallDir $cliLocalInstallFolder
+}
+
+if ($build -eq "YES")
+{
+    Write-Host ""
+    Write-Host "============================"
+    Write-Host "Update project.json"
+    Write-Host ""
+    $rootNode = $buildConfiguration.projects
+    $projects = $buildConfiguration.SelectNodes("root/projects/src/project");
+    foreach($project in $projects) {
+        $name = $project.name;
+        SetCoreFxVersion "$root\src\$name\project.json" "-*" $coreFxVersion;
+    }
 }
 
 if ($restore -eq "YES")
@@ -65,6 +91,7 @@ if ($build -eq "YES")
     $projects = $buildConfiguration.SelectNodes("projects/src/project");
     foreach($project in $projects) {
         $name = $project.name;
+        SetCoreFxVersion("src\$name\project.json", "ver-*", $coreFxVersion)
         Write-Host "Start-Process -wait -NoNewWindow $dotnetexe pack --no-build src\$name --configuration $buildConfiguration"
         Write-Host ""
         Start-Process -wait -NoNewWindow $dotnetexe "build src\$name --configuration $buildConfiguration"
