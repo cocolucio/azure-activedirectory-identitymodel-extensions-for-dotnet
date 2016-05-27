3fd9d7841b0a62030515392eb47961c59854661e
ChenYan98 <chenyan98@outlook.com>
2016-05-27 11:20:30 -0700

    Add comments for OpenIdConnect

--- src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectResponseType.cs
+++ src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectResponseType.cs
@@ -30,12 +30,27 @@ namespace Microsoft.IdentityModel.Protocols.OpenIdConnect
     /// <summary>
     /// Response types for OpenIdConnect.
     /// </summary>
+    /// <remarks>Can be used to determine the message type by consumers of an <see cref="OpenIdConnectMessage"/>.
+    /// For example: OpenIdConnectMessageTests.Publics() sets <see cref="OpenIdConnectMessage.ResponseType"/>
+    /// to <see cref="OpenIdConnectResponseType.CodeIdToken"/>.</remarks>
     public static class OpenIdConnectResponseType
     {
-#pragma warning disable 1591
+        /// <summary>
+        /// Indicates code id_token type see: http://openid.net/specs/openid-connect-core-1_0.html#HybridAuthRequest.
+        /// For Example: http://openid.net/specs/openid-connect-core-1_0.html#code-id_tokenExample.
+        /// </summary>
         public const string CodeIdToken = "code id_token";
+
+        /// <summary>
+        /// Indicates id_token type see: http://openid.net/specs/openid-connect-core-1_0.html#HybridAuthRequest.
+        /// For Example: http://openid.net/specs/openid-connect-core-1_0.html#id_tokenExample.
+        /// </summary>
         public const string IdToken = "id_token";
+
+        /// <summary>
+        /// Indicates code type see: http://openid.net/specs/openid-connect-core-1_0.html#HybridAuthRequest.
+        /// For Example: http://openid.net/specs/openid-connect-core-1_0.html#codeExample.
+        /// </summary>
         public const string Code = "code";
-#pragma warning restore 1591
     }
 }
--- src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectScope.cs
+++ src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectScope.cs
@@ -30,12 +30,24 @@ namespace Microsoft.IdentityModel.Protocols.OpenIdConnect
     /// <summary>
     /// Specific scope values that are interesting to OpenID Connect.  See http://openid.net/specs/openid-connect-messages-1_0.html#scopes
     /// </summary>
+    /// <remarks>Can be used to determine the scope by consumers of an <see cref="OpenIdConnectMessage"/>.
+    /// For example: OpenIdConnectMessageTests.Publics() sets <see cref="OpenIdConnectMessage.Scope"/>
+    /// to <see cref="OpenIdConnectScope.OpenIdProfile"/>.</remarks>
     public static class OpenIdConnectScope
     {
-#pragma warning disable 1591
+        /// <summary>
+        /// Indicates openid scope see: http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims.
+        /// </summary>
         public const string OpenId = "openid";
+
+        /// <summary>
+        /// Indicates openid profile scope see: http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims.
+        /// </summary>
         public const string OpenIdProfile = "openid profile";
+
+        /// <summary>
+        /// Indicates user_impersonation scope see: http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims.
+        /// </summary>
         public const string UserImpersonation = "user_impersonation";
-#pragma warning restore 1591
     }
 }
