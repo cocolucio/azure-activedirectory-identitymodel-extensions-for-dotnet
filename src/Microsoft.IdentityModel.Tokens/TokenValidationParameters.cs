9b2e442afb4a9eac03dbf70b2ba78cbe26a88ba4
Tushar Gupta <tushargupta51@gmail.com>
2016-05-26 19:39:15 -0700

    adding tests

--- src/Microsoft.IdentityModel.Tokens/TokenValidationParameters.cs
+++ src/Microsoft.IdentityModel.Tokens/TokenValidationParameters.cs
@@ -55,7 +55,7 @@ namespace Microsoft.IdentityModel.Tokens
     /// <summary>
     /// Definition for IssuerSigningKeyValidator.
     /// </summary>
-    /// <param name="securityKey">The <see cref="SecurityKey"/> that signed the <see cref="SecurityToken"/>./param>
+    /// <param name="securityKey">The <see cref="SecurityKey"/> that signed the <see cref="SecurityToken"/>.</param>
     /// <param name="securityToken">The <see cref="SecurityToken"/> being validated.</param>
     /// <param name="validationParameters"><see cref="TokenValidationParameters"/> required for validation.</param>
     public delegate bool IssuerSigningKeyValidator(SecurityKey securityKey, SecurityToken securityToken, TokenValidationParameters validationParameters);
